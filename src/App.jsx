2e09bee7c54db4a34244bf430d79888ed481d17c
saadman.sayeed <saadman.sayeed@bjitgroup.com>
2025-03-12 19:36:20 +0600

    added react router and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router'
+import Home from './components/pages/Home/Home'
+import NoMatch from './components/pages/NoMatch/NoMatch'
+
+function App() {
+ 
+
+  return (
+    <>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/products" element={<Products />} /> 
+            <Route path="/about" element={<About />} /> */}
+          <Route path="*" element={<NoMatch />} />
+         </Routes>
+      
+    </>
+  )
+}
+
+export default App
